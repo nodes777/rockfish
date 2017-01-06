3d325adedb2085182b0d5e2be8c7e374630a4d8a
nodes777 <nodes777@gmail.com>
2017-01-06 12:04:13 -0800

    fixes bug where rockfish would appear more than once, adds canary and vermilion rockfish

--- js/ang.js
+++ js/ang.js
@@ -2,7 +2,8 @@ angular.module("root", [])
     .controller("index", ["$scope", function($scope){
 
     	/*variables*/
-        var animalPool = [];
+        let animalPool = [];
+        let currentAnimal = {};
         $scope.guess;
 
         /*init*/
@@ -13,19 +14,25 @@ angular.module("root", [])
         /*funcs*/
 
         function getAnimal() {
-            /*Get First Animal*/
-            var randNum = Math.floor(Math.random() * animalPool.length);
-            animal = animals[randNum];
-            animalPool.splice(randNum, 1)
+            console.log(animalPool.map(function(animal) {return animal.name;}));
+            if (animalPool.length > 0) {
+	            let randNum = Math.floor(Math.random() * animalPool.length);
+	            currentAnimal = new Animal(animalPool[randNum]);
+	            animalPool.splice(randNum, 1)
+        	} else {
+        		console.log("I'm out of rockfish!")
+        	}
         };
 
         function setAnimal() {
-            $scope.img = animal.img;
+        	$scope.name = currentAnimal.name;
+            $scope.img = currentAnimal.img;
         }
 
         function Animal(data) {
-            this.name = data.name
+            this.name = data.name;
             this.img = data.img;
+            this.alts = data.alts;
         };
         function createPool () {
             /*From the animal service*/
@@ -34,6 +41,32 @@ angular.module("root", [])
             });
         }
 
-        $scope.guess;
+        $scope.compare = function(guess) {
+            guess = guess.replace(/\s/g, '').toLowerCase();
+            answer = $scope.name.replace(/\s/g, '').toLowerCase();
+            let alts = currentAnimal.alts.toLowerCase();
+            console.log("Guess: "+ guess+"\n");
+            console.log("Answer: "+ answer+"\n");
+
+            if(guess === answer){
+            	guessIsCorrect();
+            } else {
+            	guessIsWrong();
+            }
+        }
+
+        function guessIsCorrect() {
+        	console.log("Correct!");
+        	getAnimal();
+        	setAnimal();
+        	$scope.guess= '';
+        	console.log(currentAnimal);
+        	console.log($scope.img);
+        }
+
+        function guessIsWrong() {
+        	console.log("Wrong answer man");
+        	$scope.guess= '';
+        }
 
     }])
--- js/animals.js
+++ js/animals.js
@@ -2,14 +2,24 @@ let animals = [
     {
         name: "Blue Rockfish",
         img: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Sebastes_mystinus_2.jpg/220px-Sebastes_mystinus_2.jpg",
-        description: ""
+       alts: "blue"
     }, {
         name: "Black Rockfish",
         img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Sebastes_melanops.jpg/220px-Sebastes_melanops.jpg",
-        description: ""
+        alts: "Black"
     }, {
         name: "Brown Rockfish",
         img: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Brown_rockfish.jpg/220px-Brown_rockfish.jpg",
-        description: ""
+        alts: "Brown"
+    },
+    {
+        name: "Vermilion Rockfish",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Sebastes_miniatus.jpg/220px-Sebastes_miniatus.jpg",
+        alts: "Vermilion"
+    },
+    {
+        name: "Canary Rockfish",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Sebastes_pinniger_1.jpg/250px-Sebastes_pinniger_1.jpg",
+        alts: "Canary"
     },
 ];
