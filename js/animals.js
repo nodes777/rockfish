baead47389cd0f9ae2a47a6ea55173995747ea21
nodes777 <nodes777@gmail.com>
2017-01-15 21:38:20 -0800

    adds all the rockfish

--- js/animals.js
+++ js/animals.js
@@ -57,4 +57,68 @@ var animals = [
         alts: ["Bocaccio"],
         description : "The southern distinct population segment of bocaccio is a U.S. National Marine Fisheries Service Species of Concern. National Marine Fisheries Service listed the Puget Sound/Georgia basin distinct population segment as endangered on April 28, 2010. Critical habitat was designated on November 13, 2014."
     },
+    {
+        name: "Tiger Rockfish",
+        img: ["https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Sebastes_nigrocinctus.jpg/220px-Sebastes_nigrocinctus.jpg"],
+        alts: ["tiger", "tiger seaperch", "black-banded rockfish", "banded rockfish", "Sebastes nigrocinctus"],
+        description : "Scientists have dated some fish to be up to 166 years old. It is very vulnerable, with a minimum population doubling time of 14 years."
+    },
+    {
+        name: "Quillback Rockfish",
+        img: ["https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Quillback_Rockfish_Alaska_Sealife_Center.JPG/220px-Quillback_Rockfish_Alaska_Sealife_Center.JPG", "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Quillback_Rockfish.JPG/320px-Quillback_Rockfish.JPG"],
+        alts: ["Quillback seaperch", "Quillback", "Sebastes maliger"],
+        description : "Quillbacks obtain their name from the sharp, venomous quills or spines on the dorsal fin. At the base of the spines are venomous glands, which excrete poison into the spines. The stinging spines protect the quillback from predators. They are not extremely toxic to humans but can still cause pain and infection."
+    },
+    {
+        name: "Yelloweye rockfish",
+        img: ["https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Sebastes_ruberrimus_1.jpg/220px-Sebastes_ruberrimus_1.jpg"],
+        alts: ["Sebastes ruberrimus", "Yelloweye"],
+        description : "A stock assessment of the species, which incorporated data gathered from northern California and Oregon, was done in 2001. The study concluded the fish's numbers are just 7% of what they would be without human intervention in northern California, and a slightly higher 13% in Oregon. The assessment also showed a 30-year decline in numbers. These numbers are far below the 25% threshold at which a fish is labeled overfished."
+    },
+    {
+        name: "Redbanded rockfish",
+        img: ["https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Sebastes_babcocki.jpg/220px-Sebastes_babcocki.jpg"],
+        alts: ["Redbanded", "bandit", "barber pole", "flag rockfish", "Spanish flag", "hollywood", "convict" , "flag", "Sebastes babcocki"],
+        description : "Like other rockfish, this species is viviparous. The female releases the live young between March and September across the species' range."
+    },
+    {
+        name: "Yellowtail rockfish",
+        img: ["https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Sebastes_flavidus.jpg/220px-Sebastes_flavidus.jpg"],
+        alts: ["Yellowtail", "Sebastes flavidus", "Yellowtail seaperch"],
+        description : "The yellowtail rockfish occurs from San Diego, California, to Kodiak Island, Alaska; it is caught commercially off the coast of British Columbia, as well as being caught by recreational anglers off of central and northern California."
+    },
+    {
+        name: "Starry rockfish",
+        img: ["https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Starryrockfish_300.jpg/220px-Starryrockfish_300.jpg"],
+        alts: ["Starry", "spotted corsair", "spotted rockfish", "chinafish", "red rock cod", "Sebastes constellatus"],
+        description : "The starry rockfish (Sebastes constellatus) is a type of rockfish (Sebastidae) that lives mainly off the coasts of California and Baja California. This fish are also commonly called spotted corsair, spotted rockfish, chinafish, and red rock cod."
+    },
+    {
+        name: "Grass rockfish",
+        img: ["https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Sebastes_rastrelliger.jpg/220px-Sebastes_rastrelliger.jpg"],
+        alts: ["Grass", "Sebastes rastrelliger", "Grass Rockcod", "Grass Bass", "Grassy"],
+        description : "Lives in the benthic zone and feeds on crustaceans and small fish. This fish is an ambush predator and waits hiding in kelp, rocks and holes until prey passes by."
+    },
+    {
+        //Not from Wikipedia
+        name: "Rosy rockfish",
+        img: ["http://www.chabotcollege.edu/faculty/jyasaki/photogallery/fishpics/800X600/P1010047aa.jpg"],
+        alts: ["rosy", "Sebastes rosaceus"],
+        description : "If a hungry fish or seal comes by, the rockfishes duck between the boulders. But mostly they just hang there, snacking on the smaller fishes, octopuses and shrimp living among the rocks."
+    },
+    {
+        //Not from Wikipedia
+        name: "Olive rockfish",
+        img: ["http://www.fishbase.org/images/species/Seser_u0.jpg"],
+        alts: ["Sebastes serranoides", "olive"],
+        description : "The body of the olive rockfish is elongate and compressed. The upper profile of the head is almost straight, and the snout is long and pointed. The lower jaw is projecting. The olive rockfish always has nine soft rays in the anal fin; the yellowtail rockfish usually has eight."
+    },
+    {
+        //Not from Wikipedia
+        name: "Honeycomb rockfish",
+        img: ["http://www.fishbase.org/images/species/Seumb_u0.jpg", "http://friendsoflajollashores.com/marinelife/wp-content/gallery/cache/858__580x_05-honeycomb-rockfish_7669261500_o.jpg"],
+        alts: ["Sebastes umbrosus", "Honeycomb"],
+        description : "Unafraid, can usually be closely approached with slow nonthreatening movements. Very little is known about the biology of this dwarf species"
+    },
+
 ];
